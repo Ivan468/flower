f98d6e71bdd55ff9d18a86c35d6f92156c23fd06
Ivan <pig1995z@gmail.com>
2021-04-17 00:26:34 +0300

    block news finish

---
+++ skins/aridius_news_layout.php
@@ -0,0 +1,31 @@
+<?php
+// Heading 
+$_['heading_title']   		        = 'Форум';
+$_['heading_title_news_lay']   		= 'Форум';
+
+// Text
+$_['text_error']      		        = 'Нет новостей';
+$_['text_more']  			        = 'Читать далее';
+$_['text_limit']  			        = 'Показывать:';
+$_['text_posted'] 		            = 'Posted: ';
+$_['text_viewed'] 		            = '(%s views) ';
+$_['text_sort']                     = 'Показывать:';
+$_['entry_name']                    = 'Ваше имя:';
+$_['entry_review']                  = 'Ваш комментарий:';
+$_['text_login']                    = 'Пожалуйста <a href="%s"> авторизуйтесь</a> или <a href="%s"> зарегистрируйтесь</a> чтобы оставить свой комментарий';
+
+// Buttons
+$_['button_news']    	            = 'Перейти к списку новостей';
+$_['button_continue'] 	            = 'Отправить';
+$_['text_tax']	                    = 'Без налога:';
+$_['text_news_review']	       		= 'Комментарии';
+$_['text_products_related']	        = 'Рекомендуемые';
+$_['text_aridius_news_related']    	= 'Похожие новости';
+$_['button_quickview']              = 'Быстрый просмотр';
+$_['button_instock']                = 'Сообщить когда появится ';
+
+
+
+
+
+
