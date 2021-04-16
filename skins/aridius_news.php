07c1e4d3ff9a9ff2ec218c49644a3d9e0c31f358
Ivan <pig1995z@gmail.com>
2021-04-16 23:51:07 +0300

    news edit

---
+++ skins/aridius_news.php
@@ -0,0 +1,10 @@
+<?php
+// Heading 
+$_['heading_title_news']	    = 'Форум';
+
+// Text
+$_['text_more']  		= 'Читать далее';
+$_['text_posted']		= 'Posted:';
+
+// Buttons
+$_['buttonlist']     	= 'Посмотреть все статьи';
